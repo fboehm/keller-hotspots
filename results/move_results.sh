922ed636f5614fc2c0a0051c89898b75ae093045
Frederick Boehm <frederick.boehm@gmail.com>
2019-06-10 10:00:47 -0500

    moved results to run-specific subdirs

---
+++ results/move_results.sh
@@ -0,0 +1,40 @@
+mv pvl-run9931_1*.txt pvl-run-9931/.
+mv pvl-run9931_2*.txt pvl-run-9931/.
+mv pvl-run9931_3*.txt pvl-run-9931/.
+mv pvl-run9931_4*.txt pvl-run-9931/.
+mv pvl-run9931_5*.txt pvl-run-9931/.
+mv pvl-run9931_6*.txt pvl-run-9931/.
+mv pvl-run9931_7*.txt pvl-run-9931/.
+mv pvl-run9931_8*.txt pvl-run-9931/.
+mv pvl-run9931_9*.txt pvl-run-9931/.
+
+mv pvl-run9921_1*.txt pvl-run-9921/.
+mv pvl-run9921_2*.txt pvl-run-9921/.
+mv pvl-run9921_3*.txt pvl-run-9921/.
+mv pvl-run9921_4*.txt pvl-run-9921/.
+mv pvl-run9921_5*.txt pvl-run-9921/.
+mv pvl-run9921_6*.txt pvl-run-9921/.
+mv pvl-run9921_7*.txt pvl-run-9921/.
+mv pvl-run9921_8*.txt pvl-run-9921/.
+mv pvl-run9921_9*.txt pvl-run-9921/.
+
+mv pvl-run9911_1*.txt pvl-run-9911/.
+mv pvl-run9911_2*.txt pvl-run-9911/.
+mv pvl-run9911_3*.txt pvl-run-9911/.
+mv pvl-run9911_4*.txt pvl-run-9911/.
+mv pvl-run9911_5*.txt pvl-run-9911/.
+mv pvl-run9911_6*.txt pvl-run-9911/.
+mv pvl-run9911_7*.txt pvl-run-9911/.
+mv pvl-run9911_8*.txt pvl-run-9911/.
+mv pvl-run9911_9*.txt pvl-run-9911/.
+
+mv pvl-run9901_1*.txt pvl-run-9901/.
+mv pvl-run9901_2*.txt pvl-run-9901/.
+mv pvl-run9901_3*.txt pvl-run-9901/.
+mv pvl-run9901_4*.txt pvl-run-9901/.
+mv pvl-run9901_5*.txt pvl-run-9901/.
+mv pvl-run9901_6*.txt pvl-run-9901/.
+mv pvl-run9901_7*.txt pvl-run-9901/.
+mv pvl-run9901_8*.txt pvl-run-9901/.
+mv pvl-run9901_9*.txt pvl-run-9901/.
+
